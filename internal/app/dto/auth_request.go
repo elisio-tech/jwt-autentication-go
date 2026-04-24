256f2c4958961f1b43b14b0269dd6dd7110e9f8a
Elisio Augusto <elisiouiux@gmail.com>
2026-04-24 16:02:46 +0100

    auth request done

--- internal/app/dto/auth_request.go
+++ internal/app/dto/auth_request.go
@@ -2,10 +2,11 @@ package dto
 
 type RegisterRequest struct {
 	UserName string `json:"username" binding:"required"`
+	Email    string `json:"email" binding:"required"`
 	Password string `json:"password" binding:"required,min=8"`
 }
 
 type LoginRequest struct {
-	UserName string `json:"username" binding:"required"`
+	Email    string `json:"email" binding:"required"`
 	Password string `json:"password" binding:"required"`
 }
